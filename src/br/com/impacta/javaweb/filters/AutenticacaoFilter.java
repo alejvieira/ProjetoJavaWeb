d7e582dafd2a5f57a574db64867ddd02bbdbeef0
alejvieira <alejvieira@hotmail.com>
2020-03-06 21:00:10 -0300

    Create AutenticacaoFilter.java
    Exercicio 5 - Filters

---
+++ src/br/com/impacta/javaweb/filters/AutenticacaoFilter.java
@@ -0,0 +1,52 @@
+package br.com.impacta.javaweb.filters;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.impacta.javaweb.pojo.Usuario;
+
+@WebFilter(
+		dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, 
+		urlPatterns = { "/sistema"}
+		)
+public class AutenticacaoFilter implements Filter {
+
+    public AutenticacaoFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httprequest = (HttpServletRequest)request;
+		
+		Usuario usuario = (Usuario) httprequest.getSession().getAttribute("usuario");
+		
+		if (usuario != null){
+			chain.doFilter(request, response);
+		}
+		else{
+			((HttpServletResponse) response).sendRedirect("/ProjetoJavaWeb/login.html");
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
